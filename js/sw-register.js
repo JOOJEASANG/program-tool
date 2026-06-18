7b2669026761e1925d89ec860995364115f7eceb
주재상 <joojeasang@gmail.com>
2026-06-18 11:38:22 +0900

    Bump preflight compression result button cache

--- js/sw-register.js
+++ js/sw-register.js
@@ -36,7 +36,7 @@
     }
     if (location.pathname.endsWith('/tools/pdf-Checker.html') || location.pathname.endsWith('/tools/preflight.html')) {
       loadScript('preflightFixDownloadScript', '/js/preflight/fix-download.js?v=20260618-3');
-      loadScript('preflightCompressHelperScript', '/js/preflight/compress-helper.js?v=20260618-1');
+      loadScript('preflightCompressHelperScript', '/js/preflight/compress-helper.js?v=20260618-2');
       loadScript('pdfCheckerReportHelperScript', '/js/preflight/report-helper.js?v=20260518-1');
     }
   }
