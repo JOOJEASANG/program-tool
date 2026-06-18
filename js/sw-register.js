91cd28f6de8ce3921ca41f79c45a52fbd0fd21dc
주재상 <joojeasang@gmail.com>
2026-06-18 11:45:57 +0900

    Load direct result compression watcher

--- js/sw-register.js
+++ js/sw-register.js
@@ -37,6 +37,7 @@
     if (location.pathname.endsWith('/tools/pdf-Checker.html') || location.pathname.endsWith('/tools/preflight.html')) {
       loadScript('preflightFixDownloadScript', '/js/preflight/fix-download.js?v=20260618-3');
       loadScript('preflightCompressHelperScript', '/js/preflight/compress-helper.js?v=20260618-2');
+      loadScript('preflightResultCompressButtonScript', '/js/preflight/result-compress-button.js?v=20260618-1');
       loadScript('pdfCheckerReportHelperScript', '/js/preflight/report-helper.js?v=20260518-1');
     }
   }
