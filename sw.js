b14612ea0958f0c1c3e9c3cce115d0f59b6c67f9
주재상 <joojeasang@gmail.com>
2026-07-10 13:29:58 +0900

    Update service worker version for reset placement

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026-07-10-ocr-removed-v1';
+const APP_VERSION = '2026-07-10-pdf-reset-below-actions-v1';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
