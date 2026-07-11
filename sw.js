ee0d58d90acc8ec675c4441f20b5565b0878575e
주재상 <joojeasang@gmail.com>
2026-07-11 21:22:10 +0900

    chore: activate unified print workspace cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026-07-10-pdf-reset-below-actions-v1';
+const APP_VERSION = '2026-07-11-unified-print-workspace-v1';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
