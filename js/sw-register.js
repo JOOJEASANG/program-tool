4854209ce3d30d12e0d438a1c046b20b2bb34a31
주재상 <joojeasang@gmail.com>
2026-07-22 14:45:07 +0900

    chore: load cover text zones v3

--- js/sw-register.js
+++ js/sw-register.js
@@ -48,7 +48,7 @@
 
     if (isToolPath('perfect-binding-cover')) {
       loadScript('perfectBindingFineControlsScript', '/js/perfect-binding-cover-fine-controls.js?v=20260722-7');
-      loadScript('coverTextZonesScriptV2', '/js/cover-editor-text-zones-v2.js?v=20260722-2');
+      loadScript('coverTextZonesScriptV3', '/js/cover-editor-text-zones-v2.js?v=20260722-3');
     }
   }
 
