ee82e4c7e95cbef55d306debda75a77a5be1a9d8
주재상 <joojeasang@gmail.com>
2026-08-05 20:56:24 +0900

    책표지 UI 런타임 정규화 모듈 연결

--- js/sw-register.js
+++ js/sw-register.js
@@ -102,6 +102,7 @@
       tasks.push(load('coverProjectStateBridgeScriptV1','/js/cover-project-state-bridge.js?v='+VERSION));
       tasks.push(load('coverTemplateProjectSafetyScriptV2','/js/cover-template-project-safety.js?v=20260805-1'));
       tasks.push(load('coverFloatingActionDockScriptV1','/js/cover-floating-action-dock.js?v='+VERSION));
+      tasks.push(load('coverUiRuntimeNormalizerScriptV1','/js/cover-ui-runtime-normalizer.js?v=20260805-1'));
       tasks.push(load('coverRuntimeSafetyScriptV1','/js/cover-runtime-safety.js?v=20260805-1'));
     }
     return Promise.allSettled(tasks);
