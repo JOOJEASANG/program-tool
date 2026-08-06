b2e470c95299bfcc0460ce28ccfbfd2c1530d2f6
주재상 <joojeasang@gmail.com>
2026-08-06 12:55:26 +0900

    책표지 배치 잠금 런타임 연결

--- js/sw-register.js
+++ js/sw-register.js
@@ -104,6 +104,7 @@
       tasks.push(load('coverTemplateSurfaceCleanupScriptV1','/js/cover-template-surface-cleanup.js?v=20260806-1'));
       tasks.push(load('coverImagePrintQualityScriptV1','/js/cover-image-print-quality.js?v=20260806-1'));
       tasks.push(load('coverFinalOutputConfirmScriptV1','/js/cover-final-output-confirm.js?v=20260806-1'));
+      tasks.push(load('coverLayoutLockScriptV1','/js/cover-layout-lock.js?v=20260806-1'));
       tasks.push(load('coverFloatingActionDockScriptV2','/js/cover-floating-action-dock.js?v=20260806-1'));
       tasks.push(load('coverUiRuntimeNormalizerScriptV1','/js/cover-ui-runtime-normalizer.js?v=20260805-1'));
       tasks.push(load('coverOutputPerformanceSafetyScriptV1','/js/cover-output-performance-safety.js?v=20260805-1'));
