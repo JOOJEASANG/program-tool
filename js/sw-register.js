930901715e16857f07b92466beba8b24ae0817f2
주재상 <joojeasang@gmail.com>
2026-08-21 22:54:47 +0900

    feat: load design mode switch save safety

--- js/sw-register.js
+++ js/sw-register.js
@@ -106,6 +106,7 @@
           .then(()=>load('designEditorEmbeddedPolishScriptV1','/js/design-editor/phase6-embedded-polish.js?v=20260821-1'))
           .then(()=>load('designEditorRecentDraftsScriptV1','/js/design-editor/phase7-recent-drafts.js?v=20260821-1'))
           .then(()=>load('designEditorCurrentDraftResetScriptV1','/js/design-editor/phase8-current-draft-reset.js?v=20260821-1'))
+          .then(()=>load('designEditorModeSwitchSafetyScriptV1','/js/design-editor/phase9-mode-switch-safety.js?v=20260821-1'))
           .then(()=>load('designEditorPhase2ScriptV1','/js/design-editor/phase2.js?v=20260821-1'))
           .then(()=>load('designEditorOutputScriptV1','/js/design-editor/output.js?v=20260821-1'))
           .then(()=>load('designEditorPhase3ControlsScriptV1','/js/design-editor/phase3-controls.js?v=20260821-1'))
