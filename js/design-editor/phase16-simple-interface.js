31d24504bf6448b71572010b38f24b5285fb9b8a
주재상 <joojeasang@gmail.com>
2026-08-23 07:49:07 +0900

    31단계: 간단 UI 실제 일반 편집기 경로 수정

--- js/design-editor/phase16-simple-interface.js
+++ js/design-editor/phase16-simple-interface.js
@@ -4,7 +4,7 @@
   window.__designEditorSimpleInterfaceV1=true;
 
   const path=location.pathname.replace(/\/+$/,'')||'/';
-  if(path!=='/design-editor'&&path!=='/design-editor/index.html'&&!path.endsWith('/design-editor/index.html'))return;
+  if(path!=='/design-editor/general'&&path!=='/design-editor/general.html'&&!path.endsWith('/design-editor/general.html'))return;
 
   const STYLE_ID='designEditorSimpleInterfaceStyles';
   const ADVANCED_CARD_ID='designAdvancedTools';
