fde29a7145c884d08ec12799b912ad39c8f556a5
주재상 <joojeasang@gmail.com>
2026-08-27 02:19:43 +0900

    정밀 타이포그래피 런타임 로드

--- js/app-boot-guard.js
+++ js/app-boot-guard.js
@@ -27,6 +27,7 @@
   }
 
   loadDesignRuntimeScript('designTextAutoFitScriptV1','/js/design-editor/text-auto-fit.js?v=20260826-2');
+  loadDesignRuntimeScript('designTypographyProScriptV1','/js/design-editor/typography-pro.js?v=20260827-1');
   loadDesignRuntimeScript('designPrintProductionStage2ScriptV1','/js/design-editor/print-production-stage2.js?v=20260826-1');
 
   root.classList.add('app-booting');
