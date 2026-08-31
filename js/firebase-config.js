738dc75a7318fad444904d636601cd38ac3a49b4
주재상 <joojeasang@gmail.com>
2026-08-31 13:45:32 +0900

    공통 런타임 부트스트랩 버전 동기화

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -47,7 +47,7 @@ window.firebaseConfig = firebaseConfig;
   if (document.getElementById('programStudioCacheBootstrap')) return;
   const script = document.createElement('script');
   script.id = 'programStudioCacheBootstrap';
-  script.src = '/js/sw-register.js?v=2026.08.26.001';
+  script.src = '/js/sw-register.js?v=2026.08.31.003';
   script.defer = true;
   document.head.appendChild(script);
 })();
