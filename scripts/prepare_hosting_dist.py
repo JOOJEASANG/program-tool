db497158b3e423d433ced047175d6388f79401ee
주재상 <joojeasang@gmail.com>
2026-09-10 11:55:12 +0900

    Bust home launcher cache for manual modal

--- scripts/prepare_hosting_dist.py
+++ scripts/prepare_hosting_dist.py
@@ -89,7 +89,7 @@ PDF_SUITE_DAILY_FREE_SNIPPET = (
     f'<script {PDF_SUITE_DAILY_FREE_MARKER} src="/js/pdf-daily-free.js?v=20260907-2"></script>'
 )
 PDF_SUITE_HOME_SNIPPET = (
-    f'<script {PDF_SUITE_HOME_MARKER} defer src="/js/pdf-suite-home-launcher.js?v=20260910-2"></script>'
+    f'<script {PDF_SUITE_HOME_MARKER} defer src="/js/pdf-suite-home-launcher.js?v=20260910-3"></script>'
 )
 PDF_SUITE_ADVANCED_SNIPPET = (
     f'<script {PDF_SUITE_ADVANCED_MARKER} defer '
